b4fca3d0d4ff1479b25cfad2bb8b7838b7ec1863
Giovanni Cavanna <giovanni.cacciacavanna@gmail.com>
2023-12-02 18:22:45 +0100

    rotate

--- src/mlx/rotate.c
+++ src/mlx/rotate.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adi-nata <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gcavanna <gcavanna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 16:37:42 by adi-nata          #+#    #+#             */
-/*   Updated: 2023/12/02 17:46:19 by adi-nata         ###   ########.fr       */
+/*   Updated: 2023/12/02 18:19:49 by gcavanna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	rotate_dx(t_cube *cube)
+/* void	rotate_dx(t_cube *cube)
 {
 	double	dir_x;
 	double	plane_x;
 
 	dir_x = cube->player.dir.x;
-	cube->player.dir.x = cube->player.dir.x * cos((-1) * ROT_SPEED)
-						- cube->player.dir.y * sin((-1) * ROT_SPEED);
-	cube->player.dir.y = dir_x * sin((-1) * ROT_SPEED) + cube->player.dir.y
-						* cos((-1) * ROT_SPEED);
+	cube->player.dir.x = cube->player.dir.x * cos((-1) * MOV_SPEED)
+						- cube->player.dir.y * sin((-1) * MOV_SPEED);
+	cube->player.dir.y = dir_x * sin((-1) * MOV_SPEED) + cube->player.dir.y
+						* cos((-1) * MOV_SPEED);
 	plane_x = cube->player.plane.x;
-	cube->player.plane.x = cube->player.plane.x * cos((-1) * ROT_SPEED)
-							- cube->player.plane.y * sin((-1) * ROT_SPEED);
-	cube->player.plane.y = plane_x * sin((-1) * ROT_SPEED) 
+	cube->player.plane.x = cube->player.plane.x * cos((-1) * MOV_SPEED)
+							- cube->player.plane.y * sin((-1) * MOV_SPEED);
+	cube->player.plane.y = plane_x * sin((-1) * MOV_SPEED) 
 							- cube->player.plane.y;
 
 }
@@ -36,13 +36,46 @@ void	rotate_sx(t_cube *cube)
 	double	plane_x;
 
 	dir_x = cube->player.dir.x;
-	cube->player.dir.x = cube->player.dir.x * cos(ROT_SPEED)
-						- cube->player.dir.y * sin(ROT_SPEED);
-	cube->player.dir.y = dir_x * sin(ROT_SPEED) + cube->player.dir.y
-						* cos(ROT_SPEED);
+	cube->player.dir.x = cube->player.dir.x * cos(MOV_SPEED)
+						- cube->player.dir.y * sin(MOV_SPEED);
+	cube->player.dir.y = dir_x * sin(MOV_SPEED) + cube->player.dir.y
+						* cos(MOV_SPEED);
 	plane_x = cube->player.plane.x;
-	cube->player.plane.x = cube->player.plane.x * cos(ROT_SPEED)
-							- cube->player.plane.y * sin(ROT_SPEED);
-	cube->player.plane.y = plane_x * sin(ROT_SPEED) 
+	cube->player.plane.x = cube->player.plane.x * cos(MOV_SPEED)
+							- cube->player.plane.y * sin(MOV_SPEED);
+	cube->player.plane.y = plane_x * sin(MOV_SPEED) 
 							- cube->player.plane.y;
+} */
+void	rotate_dx(t_cube *cube)
+{
+	double	olddirx;
+	double	oldplanex;
+
+	olddirx = cube->player.dir.x;
+	cube->player.dir.x = cube->player.dir.x * cos((-1) * ROT_SPEED)
+		- cube->player.dir.y * sin((-1) * MOV_SPEED);
+	cube->player.dir.y = olddirx * sin((-1) * MOV_SPEED) + cube->player.dir.y
+		* cos((-1) * MOV_SPEED);
+	oldplanex = cube->player.plane.x;
+	cube->player.plane.x = cube->player.plane.x * cos((-1) * MOV_SPEED)
+		- cube->player.plane.y * sin((-1) * MOV_SPEED);
+	cube->player.plane.y = oldplanex * sin((-1) * MOV_SPEED)
+		+ cube->player.plane.y * cos((-1) * MOV_SPEED);
+}
+
+void	rotate_sx(t_cube *cube)
+{
+	double	olddirx;
+	double	oldplanex;
+
+	olddirx = cube->player.dir.x;
+	cube->player.dir.x = cube->player.dir.x * cos(MOV_SPEED)
+		- cube->player.dir.y * sin(MOV_SPEED);
+	cube->player.dir.y = olddirx * sin(MOV_SPEED) + cube->player.dir.y
+		* cos(MOV_SPEED);
+	oldplanex = cube->player.plane.x;
+	cube->player.plane.x = cube->player.plane.x * cos(MOV_SPEED)
+		- cube->player.plane.y * sin(MOV_SPEED);
+	cube->player.plane.y = oldplanex * sin(MOV_SPEED)
+		+ cube->player.plane.y * cos(MOV_SPEED);
 }
